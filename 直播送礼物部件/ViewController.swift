9fb34f30952747d00c7c1aabc96637156dab700a
疯狂运转的小马达 <18600892256@163.com>
2021-04-11 01:12:32 +0800

    Initial Commit

---
+++ 直播送礼物部件/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  直播送礼物部件
+//
+//  Created by apple on 2021/4/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 直播送礼物部件Tests/_______Tests.swift
@@ -0,0 +1,33 @@
+//
+//  _______Tests.swift
+//  直播送礼物部件Tests
+//
+//  Created by apple on 2021/4/11.
+//
+
+import XCTest
+@testable import _______
+
+class _______Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
